0d72164c36847e4eb953ef3f29044ec18bbaf7b5
Anushree-Gawali <65155550+Anushree-Gawali@users.noreply.github.com>
2020-09-06 00:37:31 +0530

    Add files via upload

---
+++ Assignment9(express)/project1/src/config.js
@@ -0,0 +1,8 @@
+const DB_CONFIG = {
+    host: "localhost",
+    user: "root",
+    password: "123456789",
+    database: "anushree",
+  };
+  
+  module.exports = { DB_CONFIG };
---
+++ Assignment9(express)/project1/src/main.js
@@ -0,0 +1,5 @@
+const dbread = require("./db.read");
+const dbadd = require("./db.add");
+
+
+dbadd.addRecordWithJsonParam({fname:"anushree",city:"kalyan"}); 
